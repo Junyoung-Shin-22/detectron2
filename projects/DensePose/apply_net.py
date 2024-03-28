845e6a3905f84f67aba78d402756a8b9cd709878
Junyoung Shin <jun021222@naver.com>
2024-03-28 08:45:19 +0000

    Update apply_net.py; now --output can be set

--- projects/DensePose/apply_net.py
+++ projects/DensePose/apply_net.py
@@ -283,8 +283,8 @@ class ShowAction(InferenceAction):
         data = extractor(outputs)
         image_vis = visualizer.visualize(image, data)
         entry_idx = context["entry_idx"] + 1
-        out_fname = './data/image-densepose/' + image_fpath.split('/')[-1]
-        out_dir = './data/image-densepose'
+        out_dir = context['out_fname']
+        out_fname = os.path.join(out_dir, image_fpath.split('/')[-1])
         if len(out_dir) > 0 and not os.path.exists(out_dir):
             os.makedirs(out_dir)
         cv2.imwrite(out_fname, image_vis)
